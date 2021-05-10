e3a9df4fdad9bac5896be48c3af3651ce0b6cb9a
Alexandre Sueiro <lvx.xande@gmail.com>
2021-05-10 13:44:21 -0300

    added RDS instance

--- deploy/varibales.tf
+++ deploy/varibales.tf
@@ -9,3 +9,11 @@ variable "project" {
 variable "contact" {
   default = "trilhandosaberlabs@gmail.com"
 }
+
+variable "db_username" {
+  description = "Username for the RDS Postgres instance"
+}
+
+variable "db_password" {
+  description = "Password for the RDS postgres instance"
+}
